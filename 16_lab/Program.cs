314e05a0635b8d7544cea75bfa6774f29f1ea8b4
RomaPLETiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2023-03-05 18:46:29 +0300

    Add files via upload

---
+++ 16_lab/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+    
+    namespace Myevent
+    {
+        
+        delegate void EventHandler(ConsoleKeyInfo k);
+        
+        class ClassWithEvent
+        {
+            public event EventHandler AnyKeyPress; 
+                                                     
+            public void fire()
+            {
+                if (AnyKeyPress != null)
+                {
+                    ConsoleKeyInfo k = Console.ReadKey(true);
+                    AnyKeyPress(k);
+                }
+            }
+        }
+        class Program
+        {
+            static void handler(ConsoleKeyInfo k)
+            {
+                char c = k.KeyChar;
+                Console.WriteLine("Вiдбулася подiя: натискання клавiшi '{0}'", c);
+            if (c == 'r') {
+                Console.WriteLine("Roman Pletiuk ");
+            }
+
+            }
+            static void Main()
+            {
+                
+            ClassWithEvent evt = new ClassWithEvent(); 
+                
+            
+                evt.AnyKeyPress += new EventHandler(handler);
+               
+                evt.fire();
+                Console.ReadKey();
+            }
+        }
+    }
+
+
