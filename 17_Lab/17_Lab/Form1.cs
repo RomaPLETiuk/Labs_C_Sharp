34c536634afd097dab38d8ef7e85d37e2aca3e73
RomaPLETiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2023-03-09 16:43:25 +0300

    Add files via upload

---
+++ 17_Lab/17_Lab/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace _17_Lab
+{
+    public partial class Form1 : Form
+    {
+        XmlSerializer formater;
+        FileStream fs;
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+
+            button1.Click += button1_Click;
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);            
+            }
+            pictureBox1.Image = bmp;
+        }
+ 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int radius = int.Parse(textBox1.Text);
+            if (radius < 1)
+            {
+                radius = 100;
+            }
+          
+            int centerX = ClientRectangle.Width / 3;
+            int centerY = ClientRectangle.Height / 3; 
+            
+            RegPentagon rP = new RegPentagon(radius);
+            Point[] points = rP.GetPoints(centerX, centerY);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.DrawPolygon(Pens.Red, points);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int radius = int.Parse(textBox1.Text);
+
+            RegPentagon rP = new RegPentagon(radius);
+            formater = new XmlSerializer(typeof(RegPentagon));
+            fs = new FileStream("pentagon.xml", FileMode.OpenOrCreate);
+            formater.Serialize(fs, rP);
+            fs.Close();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegPentagon rP;
+            if (File.Exists("pentagon.xml"))
+            {
+                fs = new FileStream("pentagon.xml", FileMode.Open);
+                formater = new XmlSerializer(typeof(RegPentagon));
+                rP = (RegPentagon)formater.Deserialize(fs);
+                fs.Close();
+                //var p = rP.radius;
+                textBox1.Text = Convert.ToString(rP.radius);
+
+
+                int centerX = ClientRectangle.Width / 3;
+                int centerY = ClientRectangle.Height / 3;
+
+                
+                Point[] points = rP.GetPoints(centerX, centerY);
+
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.DrawPolygon(Pens.Red, points);
+                }
+                pictureBox1.Image = bmp;
+
+            }
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
+}
---
+++ 17_Lab/17_Lab/RegPentagon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _17_Lab
+{
+
+    [Serializable]
+    public class RegPentagon
+    {
+        public int radius;
+
+        public RegPentagon()
+        {
+            radius = 1;
+        }
+
+        public RegPentagon(int r)
+        {
+
+                radius = r;
+        }
+
+        public Point[] GetPoints(int centerX, int centerY) {
+
+           
+
+            // Визначаємо координати кутів п'ятикутника
+            Point[] points = new Point[5];
+            double angle = 0;
+            double angleIncrement = 2 * Math.PI / 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int x = (int)Math.Round(centerX + radius * Math.Cos(angle));
+                int y = (int)Math.Round(centerY + radius * Math.Sin(angle));
+                points[i] = new Point(x, y);
+                angle += angleIncrement;
+            }
+
+            return points;
+
+        }
+
+    }
+}
